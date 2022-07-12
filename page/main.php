289cf59f0f84c6cb2dc27397b906a3791b410cc6
Roman Romanov <kikemaru@mail.ru>
2022-07-12 12:22:28 +0300

    🔐Update auth page

--- page/main.php
+++ page/main.php
@@ -1,10 +1,11 @@
 <?php $this->title('Авторизация'); ?>
 <center style="margin-top: 40px;">
     <h3>Авторизация:</h3>
-    <form method="POST" action="./">
-        <input type="hidden" name="action" value="auth">
-        <p><input type="text" name="login" class="form-control" id="inputSuccess1" style="max-width: 240px;" placeholder="Логин:"></p>
-        <input type="password" name="password" class="form-control" id="inputSuccess1" style="max-width: 240px;" placeholder="Пароль:"><br>
-        <p><input type="submit" value="Войти"class="btn btn-success" style="width: 240px;"></p>
-    </form>
+    <?php
+    $this->CreateForm('post', 'auth');
+    $this->AddInput('text', 'login', 'Логин:', 'form-control', 240);
+    $this->AddInput('password', 'password', 'пароль:', 'form-control', 240);
+    $this->AddButton('Войти', 'btn btn-success', 240);
+    $this->EndForm();
+    ?>
 </center>
