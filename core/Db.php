04b69054ca5979c1167c3785ff8f73a772b8ab2f
Roman Romanov <kikemaru@mail.ru>
2022-07-14 23:01:45 +0300

    📍Add new method in Db class

--- core/Db.php
+++ core/Db.php
@@ -2,5 +2,38 @@
 
 class Db
 {
+    public $db;
 
+    public function __construct()
+    {
+        $this->db = new PDO("mysql:host=localhost;dbname=datawork;", 'root', 'root');
+    }
+
+    public function GetDb()
+    {
+        //return $this->db;
+        echo 123;
+    }
+
+
+    //Регистрация пользователя
+    public function Reg($login, $pass, $alias)
+    {
+
+    }
+
+
+    //Авторизация пользователя
+    public function Auth($login, $pass)
+    {
+        $dbc = $this->db;
+        $rs = $dbc->query("SELECT * FROM users WHERE login='$login'")->fetch();
+
+        if ($rs['password'] == $pass)
+        {
+            echo $rs['alias'];
+        } else {
+            echo 'error data';
+        }
+    }
 }
