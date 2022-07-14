8491f4831207c283af9af007742211779e43ab87
Roman Romanov <kikemaru@mail.ru>
2022-07-14 23:03:37 +0300

    🆕Add new include, and use new class

--- include/require.php
+++ include/require.php
@@ -5,4 +5,6 @@ include_once './core/Start.php';
 $start = new Start();
 include_once './core/Router.php';
 $router = new Router($rout);
+include_once './core/Db.php';
+$db = new Db();
 include_once './include/connect.php';
